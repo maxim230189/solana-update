f511c81928a3f90c6004b4b7ea85d417c3cb687d
maxim230189 <81288157+maxim230189@users.noreply.github.com>
2025-07-20 22:49:40 +0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -37,7 +37,7 @@ fi
 
 if [ $networkrpcURL = https://api.testnet.solana.com ];then
 version="$(wget -q -4 -O- https://api.margus.one/solana/version/?cluster=testnet)"
-version="2.3.4"
+version="2.3.5"
 maxdelinq=10
 mintime=10
 elif [ $networkrpcURL = https://api.mainnet-beta.solana.com ];then
